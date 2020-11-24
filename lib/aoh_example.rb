7324429f90ca019dd4fd4c8154305e15db4250e3
Diana Rybikov <diana.ryb214@gmail.com>
2020-11-24 00:41:17 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -35,5 +35,8 @@ end
 
 def aoh_update(aoh, row, key, new_value)
   local_copy_of_aoh = assembled_aoh
-  aoh[row][key] = new_value
+  
+  local_copy_of_aoh[row][key] = new_value
+ 
+  local_copy_of_aoh
 end
